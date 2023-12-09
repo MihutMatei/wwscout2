c506f24a60fedd4803eefa1a1772cab547eb24bd
fl9ppy <adi.calinescu16@gmail.com>
2023-12-09 17:08:52 +0200

    .

--- backend/wwscout_project/wwscout_app/urls.py
+++ backend/wwscout_project/wwscout_app/urls.py
@@ -1,7 +1,9 @@
+# wwscout_app/urls.py
+
 from django.urls import path
-from .views import GetJobApplicationsView
+from .views import HomeView, GetJobApplicationsView
 
 urlpatterns = [
-    # ... existing URL patterns ...
+    path('', HomeView.as_view(), name='home'),  # Add this line for the root path
     path('api/get_job_applications/', GetJobApplicationsView.as_view(), name='get_job_applications'),
 ]
