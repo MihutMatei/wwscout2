5cf48aaf2f17f1ee4180942a98e19fe43a03b8a8
fl9ppy <adi.calinescu16@gmail.com>
2023-12-10 00:44:04 +0200

    .

--- backend/wwscout_project/wwscout_app/models.py
+++ backend/wwscout_project/wwscout_app/models.py
@@ -1,12 +1,14 @@
-# wwscout_app/models.py
-
 from django.db import models
 
 class JobApplication(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    open_position = models.CharField(max_length=255)
+    num_open_positions = models.PositiveIntegerField()
+    hours_per_week = models.PositiveIntegerField()
+    days_off_per_year = models.PositiveIntegerField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    facilities = models.TextField()
+    subscription_type = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-
-class Meta:
-    app_label = 'wwscout_app'
