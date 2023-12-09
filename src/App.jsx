def06b52be1bc1f510f9f0ddcb242926b8b72f7b
fl9ppy <adi.calinescu16@gmail.com>
2023-12-09 18:17:03 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react'
 import axios from 'axios';
-import './App.css';
+import './App.css'
 
-function ListingsPage() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [jobApplications, setJobApplications] = useState([]);
+const placeholderData = [
+  { id: 1, title: 'Placeholder Title 1', description: 'Placeholder Description 1' },
+  { id: 2, title: 'Placeholder Title 2', description: 'Placeholder Description 2' },
+  { id: 3, title: 'Placeholder Title 4', description: 'Placeholder Description 3' },
+  { id: 4, title: 'Placeholder Title 3', description: 'Placeholder Description 4' },
+  { id: 5, title: 'Placeholder Title 3', description: 'Placeholder Description 5' },
+  { id: 6, title: 'Placeholder Title 3', description: 'Placeholder Description 6' },
+  { id: 7, title: 'Placeholder Title 4', description: 'Placeholder Description 7' },
+  { id: 8, title: 'Placeholder Title 4', description: 'Placeholder Description 8' },
+  { id: 9, title: 'Placeholder Title 4', description: 'Placeholder Description 9' },
+  // Add more placeholder items to create 3 rows with 3 listings each
+];
 
-  useEffect(() => {
-    // Use axios to fetch job applications from the Django endpoint
-    axios.get('http://localhost:8000/api/get_job_applications/')
-      .then(response => setJobApplications(response.data.data))
-      .catch(error => console.error('Error fetching job applications:', error));
-  }, []);
-
-  // Filter job applications based on the search term
-  const filteredData = jobApplications.filter(item =>
-    item.position.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  // Divide filtered data into chunks of 3 for each row
+function ListingsPage({ filteredData }) {
   const chunkedData = [];
   for (let i = 0; i < filteredData.length; i += 3) {
     chunkedData.push(filteredData.slice(i, i + 3));
@@ -26,21 +23,13 @@ function ListingsPage() {
 
   return (
     <div className="listings-page">
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search by position..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
       <div className="cards-container">
         {chunkedData.map((row, rowIndex) => (
           <div className="row" key={rowIndex}>
             {row.map((item, columnIndex) => (
               <div className="card" key={columnIndex}>
-                <h2>{item.position}</h2>
-                <p>{item.name}</p>
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
               </div>
             ))}
           </div>
@@ -51,12 +40,25 @@ function ListingsPage() {
 }
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const filteredData = placeholderData.filter((item) =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
       <div className="header">
         <h1 className="header-text">Job Listings</h1>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search by title..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
       </div>
-      <ListingsPage />
+      <ListingsPage filteredData={filteredData} />
     </div>
   );
 }
