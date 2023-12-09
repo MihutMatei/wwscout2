df208fdfb72337a70ae86da8a3458f2a324c82e0
fl9ppy <adi.calinescu16@gmail.com>
2023-12-09 16:56:54 +0200

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ListingsPage() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [jobApplications, setJobApplications] = useState([]);
+
+  useEffect(() => {
+    // Use axios to fetch job applications from the Django endpoint
+    axios.get('http://localhost:8000/api/get_job_applications/')
+      .then(response => setJobApplications(response.data.data))
+      .catch(error => console.error('Error fetching job applications:', error));
+  }, []);
+
+  // Filter job applications based on the search term
+  const filteredData = jobApplications.filter(item =>
+    item.position.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Divide filtered data into chunks of 3 for each row
+  const chunkedData = [];
+  for (let i = 0; i < filteredData.length; i += 3) {
+    chunkedData.push(filteredData.slice(i, i + 3));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="listings-page">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search by position..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="cards-container">
+        {chunkedData.map((row, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {row.map((item, columnIndex) => (
+              <div className="card" key={columnIndex}>
+                <h2>{item.position}</h2>
+                <p>{item.name}</p>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1 className="header-text">Job Listings</h1>
+      </div>
+      <ListingsPage />
+    </div>
+  );
+}
+
+export default App;
