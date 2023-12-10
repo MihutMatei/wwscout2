2feb40554d1214a74cf4a0d566b4428a0fa57b94
Pr0gr4mm1ng <152791491+Pr0gr4mm1ng@users.noreply.github.com>
2023-12-10 15:15:25 +0200

    Whatssapp

--- src/goodfront.jsx
+++ src/goodfront.jsx
@@ -60,7 +60,7 @@ export function ListingsPage() {
                   </Card>
                 ))}
               </Flex>
-            </Flex>
+            </Flex> 
           ))}
         </div>
       </div>
