5fc89169c5959e7cf92be7b25e60baa252c36630
Pr0gr4mm1ng <152791491+Pr0gr4mm1ng@users.noreply.github.com>
2023-12-10 03:59:58 +0200

    COAIE E GATA

--- src/Register.jsx
+++ src/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Logo from './assets/Logo.png';  // Correct the typo in the import path
-import './App.css'
+import './Register.css'
 
 function App() {
   return (
